aa62dcc923ee97ac050311bde2dce0565439f9b2
Andrew Rady <andrew.arsoftware@gmail.com>
2016-01-24 19:34:25 -0700

    Changed image sizes for uploads

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,4 @@
 class Member < ActiveRecord::Base
-	has_attached_file :image, styles: { large: "700x400>", medium: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :image, styles: { large: "1700x1000>", medium: "700x500>" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
